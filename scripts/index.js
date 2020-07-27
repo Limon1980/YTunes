63a41b55c0575544290d361a205a021832b28b07
Limon1980 <tarkalmob@yandex.ru>
2020-07-27 20:16:39 +1000

    vscode://vscode.github-authentication/did-authenticate?windowId=1&code=d097fd5eee57b2d49c1e&state=a7ffe6f2-1da0-46e3-be1e-d5706250594b

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+import { radioPlayerInit } from "./radioPlayer.js";
+import { videoPlayerInit } from "./videoPlayer.js";
+import { musicPlayerInit } from "./musicPlayer.js";
+
+const playerBtn = document.querySelectorAll(".player-btn");
+const playerBlock = document.querySelectorAll(".player-block");
+const temp = document.querySelector(".temp");
+
+const deactivationPlayer = () => {
+  playerBtn.forEach((item) => {
+    temp.style.display = "none";
+    item.classList.remove("active");
+  });
+  playerBlock.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+
+playerBtn.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    deactivationPlayer();
+    btn.classList.add("active");
+    playerBlock[i].classList.add("active");
+  });
+});
+
+console.log(playerBtn);
+console.log(playerBlock);
+
+radioPlayerInit();
+videoPlayerInit();
+musicPlayerInit();
