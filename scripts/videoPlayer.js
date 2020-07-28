8b8f2d0d10d40d62cd501178e639cded8f25c75d
Limon1980 <tarkalmob@yandex.ru>
2020-07-28 22:28:09 +1000

    public rep

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -2,13 +2,6 @@ export const videoPlayerInit = () => {
   console.log("Video Init");
 };
 
-//video-player
-//video-button__play
-//video-button__stop
-//video-time__passed
-//video-progress
-//video-time__total
-
 const videoPlayer = document.querySelector(".video-player");
 const videoButtonPlay = document.querySelector(".video-button__play");
 const videoButtonStop = document.querySelector(".video-button__stop");
